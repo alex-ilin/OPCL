7e719c3c72367871b43c6883035784ec6eeba1c5
Alexander Iljin <ajsoft@yandex.ru>
2010-12-01 11:08:23 +0600

    Add CompileAndLink.bat for those who don't have GNU Make installed.

---
+++ Samples/CompileAndLink.bat
@@ -0,0 +1,17 @@
+o2ethclc.exe Win32.Kernel32.Mod
+o2ethclc.exe Win32.Kernel.Mod
+o2ethclc.exe Win32.ADVAPI32.Mod
+o2ethclc.exe Win32.Registry.Mod
+o2ethclc.exe Win32.FileDir.Mod
+o2ethclc.exe Win32.Files.Mod
+o2ethclc.exe Win32.Modules.Mod
+o2ethclc.exe HelloCon.Mod
+o2ethclc.exe User32.Mod
+o2ethclc.exe HelloGUI.Mod
+o2ethclc.exe HelloDLL.Mod
+o2ethclc.exe HelloDLLMain.Mod
+
+o2ethcll.exe HelloCon.exe.Link
+o2ethcll.exe HelloGUI.exe.Link
+o2ethcll.exe HelloDLL.dll.Link
+o2ethcll.exe HelloDLL.exe.Link
