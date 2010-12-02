71b722ce20149303005c03f9728ea5af7f8492fb
Alexander Iljin <ajsoft@yandex.ru>
2010-12-03 01:09:52 +0600

    Add CompileAndLink.bat for the Src folder.

---
+++ Src/CompileAndLink.bat
@@ -0,0 +1,35 @@
+o2c.exe Win32.Kernel32.Mod
+o2c.exe Win32.Kernel.Mod
+o2c.exe Win32.ADVAPI32.Mod
+o2c.exe Win32.Registry.Mod
+o2c.exe Win32.FileDir.Mod
+o2c.exe Win32.Files.Mod
+o2c.exe Win32.Modules.Mod
+o2c.exe Win32.Objects.Mod
+o2c.exe Win32.Reals.Mod
+o2c.exe Dates.Mod
+o2c.exe Strings.Mod
+o2c.exe Console.Mod
+o2c.exe Win32.Fonts.Mod
+o2c.exe Texts.Mod
+o2c.exe Win32.OPM.Mod
+o2c.exe OPS.Mod
+o2c.exe OPT.Mod
+o2c.exe OPO.Mod
+o2c.exe OPB.Mod
+o2c.exe OPA.Mod
+o2c.exe OPL.Mod
+o2c.exe OPC.Mod
+o2c.exe OPP.Mod
+o2c.exe OPV.Mod
+o2c.exe CmdLine.Mod
+o2c.exe o2c.Mod
+
+o2c.exe Types.Mod
+o2c.exe Win32.Threads.Mod
+o2c.exe Win32.Exceptions.Mod
+o2c.exe Bootlink.Mod
+o2c.exe O2ETHCLL.Mod
+
+o2l.exe O2ETHCLC.exe.Link
+o2l.exe O2ETHCLL.exe.Link
